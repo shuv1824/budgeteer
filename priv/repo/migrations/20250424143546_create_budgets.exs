6b3443557290d709e0080be38dee3e51eaac208e
Shah Nawaz <shuvo1824@hotmail.com>
2025-04-24 21:00:07 +0600

    chore: budget model and migration created

---
+++ priv/repo/migrations/20250424143546_create_budgets.exs
@@ -0,0 +1,23 @@
+defmodule Budgeteer.Repo.Migrations.CreateBudgets do
+  use Ecto.Migration
+
+  def change do
+    create table(:budgets, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :description, :text
+      add :start_date, :date, null: false
+      add :end_date, :date, null: false
+      add :creator_id, references(:users, type: :binary_id, on_delete: :nothing)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:budgets, [:creator_id])
+
+    create constraint(:budgets, :budget_end_after_start,
+             check: "end_date > start_date",
+             comment: "Budget must end after its start date"
+           )
+  end
+end
